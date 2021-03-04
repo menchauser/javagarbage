f713d991c246d0ca5fa9bb76cf86a5856de7d35f
Mukhamed Karanashev <menchauser@gmail.com>
2021-03-04 13:45:17 +0300

    Parse in-memory

--- src/main/java/test/json/TestBigJson.java
+++ src/main/java/test/json/TestBigJson.java
@@ -2,8 +2,9 @@ package test.json;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.Duration;
 import java.util.Collection;
 import java.util.Map;
@@ -11,8 +12,9 @@ import java.util.Map;
 public class TestBigJson {
     public static void main(String[] args) throws IOException {
         var mapper = new ObjectMapper();
+        var text = Files.readString(Paths.get(args[0]));
         var start = System.nanoTime();
-        Map<String, ?> result = mapper.readValue(new File(args[0]), Map.class);
+        Map<String, ?> result = mapper.readValue(text, Map.class);
         var elapsed = System.nanoTime() - start;
         System.out.printf("Parsed %d messages in %s\n", ((Collection) result.get("messages")).size(), Duration.ofNanos(elapsed));
     }
